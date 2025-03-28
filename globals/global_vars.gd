95dc1095535b79cdfe8d8c3bce739978e76a6d6f
RichardMJaks <richardmiikael.jaks@gmail.com>
2025-03-28 18:09:43 +0200

    fixed magnets

--- globals/global_vars.gd
+++ globals/global_vars.gd
@@ -3,3 +3,4 @@ extends Node
 # Standard Unit
 var su: float = 30
 enum POLE {SOUTH = -1, NORTH = 1}
+var magnet_damping: float = 0.4
--- player/player_controller.gd
+++ player/player_controller.gd
@@ -18,40 +18,41 @@ var current_pole: GlobalVars.POLE = GlobalVars.POLE.NORTH
 func _ready() -> void:
 	pass
 
+func _process(_delta: float) -> void:
+	%Label.text = GlobalVars.POLE.find_key(current_pole)
+
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed(&"a_switch"):
+		@warning_ignore("int_as_enum_without_cast")
+		current_pole = current_pole * -1;
+
 	being_attracted = not magnets.is_empty()
-	var dir = Input.get_axis(&"m_left", &"m_right")
 	
-	if not being_attracted and not is_on_floor():
-		velocity.y += get_gravity().y * delta
+	_handle_movement(delta)
 
-	# Attraction
-	if being_attracted:
-		var self_pos := global_position
-		for magnet: Magnet in magnets:
-			var magnet_pos := magnet.global_position
-			var attraction_vector = self_pos - magnet_pos
-			print(attraction_vector)
-			var magnet_pole: GlobalVars.POLE = magnet.pole
-			var attraction_dir: int = magnet_pole * current_pole
+	move_and_slide()
 
-			var magnet_strength = magnet.strength * su
-
-			velocity += attraction_vector * attraction_dir * magnet_strength * delta
-
-	# Movement
-	if dir:
-		var acceleration = speed / acceleration_time
-		if abs(velocity.x) <= speed:
-			velocity.x += dir * acceleration * delta
-	else:
-		var deceleration = speed / deceleration_time
-		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
-
+func _handle_movement(delta) -> void:
 	if Input.is_action_just_pressed(&"m_jump") and is_on_floor():
 		velocity.y -= _get_jump_height(jump_height)
 
-	move_and_slide()
+	var dir = Input.get_vector(&"m_left", &"m_right", &"m_up", &"m_down")
+	var acceleration = speed / acceleration_time
+	var deceleration = speed / deceleration_time
+
+	if not is_on_floor():
+		velocity.y += get_gravity().y * delta
+
+	if not dir and not being_attracted:
+		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
+		return
+
+	if dir and abs(velocity.x) < speed:
+		velocity.x += dir.x * acceleration * delta
+
+	if not being_attracted and is_on_floor():
+		velocity.x = min(abs(velocity.x), abs(speed)) * sign(velocity.x)
+
 	
 	
 func _get_jump_height(h: float) -> float:
