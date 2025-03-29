e0cb510729a4e903d676a020c006c6f33df87484
RichardMJaks <richardmiikael.jaks@gmail.com>
2025-03-29 13:22:27 +0200

    fixed major bugs

--- Scripts/magnet_vol2/magnet_floor.gd
+++ Scripts/magnet_vol2/magnet_floor.gd
@@ -2,5 +2,19 @@ extends Magnet
 class_name FloorMagnet
 
 # Direction of gravity this floor magnet applies when opposite poles attract
-@export var magnet_gravity_direction: Vector2i = Vector2i(0, -1)  # Default is inverted gravity
+@export_enum("Left:0", "Right:1", "Up:2", "Down:3") var magnet_direction: int = 3
+var magnet_gravity_direction: Vector2i = Vector2i(0, -1):
+	get:
+		match(magnet_direction):
+			0:
+				return Vector2(1, 0)
+			1:
+				return Vector2(-1, 0)
+			2:
+				return Vector2(0, 1)
+			3:
+				return Vector2(0, -1)
+			_:
+				return Vector2(0, 0)
+		  # Default is inverted gravity
 @export var launch_power: float = 1200.0  # Power of the launch when polarity switches
