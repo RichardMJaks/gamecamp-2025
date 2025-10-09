6fdbe6873b9d9b2649ae230093e6d9709b5576dc
RichardMJaks <richardmiikael.jaks@gmail.com>
2025-10-09 18:23:07 +0300

    initial framework for end gates

--- levels/level_management/level_end_gate/end_gate.gd
+++ levels/level_management/level_end_gate/end_gate.gd
@@ -1,7 +1,22 @@
 extends Area2D
 
 signal level_completed
+var player: Player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	level_completed.connect(GameController.complete_level)
+
+
+func _on_player_entered_end_gate(body: Player) -> void:
+	var end_position = _get_player_end_position()
+	_tween_player_to_end_position(body, end_position)
+
+func _tween_player_to_end_position(player: Player, end_position: Vector2) -> void:
+	var tween = player.create_tween()
+	tween.tween_property(player, ^"global_position", end_position, 0.1) 
+
+
+func _get_player_end_position() -> Vector2:
+	return $PlayerFinalPosition.global_position
+
