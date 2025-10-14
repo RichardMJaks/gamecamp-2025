9837a5e43b6e6a72db7a44d6f9f688796a69b226
Richard Miikael Jaks <richardmiikael.jaks@gmail.com>
2025-10-14 12:11:44 +0300

    added animation to level stats

--- Scenes/level_complete_ui.gd
+++ Scenes/level_complete_ui.gd
@@ -2,11 +2,13 @@ extends PanelContainer
 
 @onready var total_collectibles_label: Label = %TotalCollectibles
 @onready var total_time_label: Label = %TotalTime
+@onready var container_revealer: AnimationPlayer = %ContainerRevealer
 
 func show_ui(total_collectibles: int, total_time: float) -> void:
 	visible = true
 	total_collectibles_label.text = str(total_collectibles)
 	total_time_label.text = GlobalVars.float_to_time(total_time) 
+	container_revealer.play("reveal")
 
 
 func _on_continue_button_pressed() -> void:
